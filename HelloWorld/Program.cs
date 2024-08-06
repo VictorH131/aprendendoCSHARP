18e8d0dde552c290fcad0aa18c379c695f4dbae6
victor.hsalmeida5 <victor.hsalmeida5@ITA0557225W10-1>
2024-08-06 16:04:40 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ola mundo");       //escreva
+            Console.Read();                   //leia
+            Console.WriteLine("ola mundo");   //escreval
+            Console.ReadKey();                //leia quarquer tecla
+
+        }
+    }
+}ed
