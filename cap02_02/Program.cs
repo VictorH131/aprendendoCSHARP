d75520447b6e27c7b6fe71e193f95d210c0ea140
Victor_131 <161103174+VictorH131@users.noreply.github.com>
2024-08-13 16:27:03 -0300

    finalização cap 2

---
+++ cap02_02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cap02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, R;     // add variavel numerica (INTEIRA)
+
+            // escrever na tela, e ler o valor 1 e 2
+            Console.Write("Entre 1 valor: ");     
+            A = int. Parse(Console.ReadLine());
+
+            Console.Write("Entre 2 valor: ");
+            B = int.Parse(Console.ReadLine());
+            
+            R = A + B;
+
+            Console.WriteLine();
+            Console.WriteLine("Resultados = " + R);
+
+
+            // comando basico de controle de finalização
+            Console.WriteLine();
+            Console.WriteLine("tecle <Enter> para encerrar... ");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ cap02_03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cap02_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float HT, VH, PD, SB, TD, SL; // add de variavel numerico ("flutuante")
+            // HT= Horas trabalhadas, VH= Valor da Hora, PD= Percentual de desconto, SB= salario bruto, TD= total descontado, SL= Salario liquido 
+            
+            // escreve na tela e le os valores "HT" "VH" e "PD"
+            Console.Write("Horas trabalhadas ..............: ");
+            HT = float.Parse(Console.ReadLine());
+            Console.Write("valor da Hora ..............: ");
+            VH = float.Parse(Console.ReadLine());
+            Console.Write("Valor do percentual de desconto .: ");
+            PD = float.Parse(Console.ReadLine());
+
+            // faz um calculo de salario liquido
+            SB = HT * VH;
+            TD = (PD / 100) * SB;
+            SL = SB - TD;
+
+            // mostra o resultado
+            Console.WriteLine();
+            Console.Write("Salario liquido .................: ");
+            Console.WriteLine(SL.ToString("##,##0,00")); // mascara  de comando
+
+            // comando basico de controle de finalização
+            Console.WriteLine();
+            Console.WriteLine("tecle <Enter> para encerrar... ");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
